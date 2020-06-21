728c3719406f595b31b3c1850b385c531e14f306
Rekas <aure369@gmail.com>
2020-06-21 15:52:20 +0300

    adding wait on exit and chmoding

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const shell = require('shelljs');
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
+const pressAnyKey = require('press-any-key');
 
 const cli = require('./lib/cli');
 const vscode = require('./lib/vscode');
@@ -15,19 +16,33 @@ console.log(
   )
 );
 
-if (!vscode.detect() || !ssh_gen.detect()) {
-    console.log(chalk.red('Sorry, this script requires vscode and ssh-keygen'));
+const anyKeyToExit = async () => {
+  try {
+    await pressAnyKey("Press any key to exit...", {
+      ctrlC: "reject"
+    });
+  }
+  finally {
     process.exit();
-}
+  }
+};
 
-if (!vscode.detectRemoteExtension()) {
-    // not found
-    // install command: code --install-extension ms-vscode-remote.remote-ssh
-    // TODO: install
-    console.log(chalk.red('Sorry, this script requires ms-vscode-remote.remote-ssh extension'));
-    process.exit();
-}else {
-    // found..
-    console.log(chalk.green('Requirements found! Proccessing...'));
-    cli.run();
-}
+(async function(){
+  if (!vscode.detect() || !ssh_gen.detect()) {
+      console.log(chalk.red('Sorry, this script requires vscode and ssh-keygen'));
+      await anyKeyToExit();
+  }
+
+  if (!vscode.detectRemoteExtension()) {
+      // not found
+      // install command: code --install-extension ms-vscode-remote.remote-ssh
+      // TODO: install
+      console.log(chalk.red('Sorry, this script requires ms-vscode-remote.remote-ssh extension'));
+      await anyKeyToExit();
+  }else {
+      // found..
+      console.log(chalk.green('Requirements found! Proccessing...'));
+      await cli.run();
+      await anyKeyToExit();
+  }
+})();
